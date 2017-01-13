8afdcbe64e450ab6c1ce15d991da83fe505e6635
Quentin Mathe <quentinx.mathe@intel.com>
2017-01-13 13:59:05 +0100

    Some preliminary work on Image implementation (doesn't compile)

--- Tapestry/Image.swift
+++ Tapestry/Image.swift
@@ -8,16 +8,38 @@
 import Foundation
 import CoreGraphics
 
+enum BitsPerComponent: UInt8 {
+    case 1
+    case 2
+    case 4
+    case 8
+    case 16
+}
+
 /// Bitmap image
+///
+/// The image data must be in a packed format, planar is unsupported.
 ///
 /// To draw an image, use 2DContext.drawImage(:in:).
 open struct Image {
     
     private var image: CGImage
-    var size: Size
-    var bitsPerComponent: Int
-    var bitsPerPixel: Int
-    var bytesPerRow: Int
+    let size: Size {
+        return Size(image.size.width, image.size.height)
+    }
+    /// The number of bits used to encode a color component.
+    ///
+    /// The represented color range increases with this value.
+    var bitsPerComponent: BitsPerComponent
+    /// The number of color components per pixel.
+    ///
+    /// For example, both CMYK (cyan, magenta, yellow, black) and RGBA (red, green, blue, alpha) use 4.
+    var componentsPerPixel: UInt8 {
+        return image.bitsPerPixel
+    }
+    var bytesPerRow: UInt {
+        return image.bytesPerRow
+    }
     var colorSpace: ColorSpace?
     var data: Data
 
@@ -25,7 +47,30 @@ open struct Image {
         return init(data: Data(url: url))
     }
     
-    init(data: Data) {
+    init(size: Size, data: Data, bitsPerComponent: BitsPerComponent, bitsPerPixel: UIInt8, bytesPerRow: UIInt) {
+        return Image(size: size,
+                 provider: provider,
+         bitsPerComponent: bitsPerComponent.rawValue,
+             bitsPerPixel: bitsPerPixel,
+              bytesPerRow: bytesPerRow)
+    }
     
+    private init(size: Size, provider: CGDataProvider, bitsPerComponent: BitsPerComponent, bitsPerPixel: UIInt8, bytesPerRow: UIInt) {
+        let info = CGBitmapInfo()
+        let provider =
+        
+            CGImage(
+        self.data = data
+        self.image = CGImage(width: size.width,
+                            height: size.height,
+                  bitsPerComponent: bitsPerComponent.rawValue,
+                      bitsPerPixel: bitsPerPixel,
+                       bytesPerRow: bytesPerRow,
+                             space: CGColorSpaceCreateDeviceRGB(),
+                        bitmapInfo: info,
+                          provider: provider,
+                            decode: nil,
+                 shouldInterpolate: true,
+                            intent: .defaultIntent)
     }
 }
