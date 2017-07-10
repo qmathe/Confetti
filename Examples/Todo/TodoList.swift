1a91e0593f7b286869f8a3a4d98b851e9f42088e
Quentin Mathé <quentin.mathe@gmail.com>
2017-07-10 15:03:31 +0200

    Wrapped Todo list UI into a root item to prevent the UI to be split into multiple windows

--- Examples/Todo/TodoList.swift
+++ Examples/Todo/TodoList.swift
@@ -17,17 +17,19 @@ class TodoList: CollectionViewpoint<Todo>, UI {
 	}
 	
 	override func itemPresentingCollection(from item: Item) -> Item {
-		return (item.items ?? [])[0]
+		return (item.items?.first?.items ?? [])[0]
 	}
 
     override func generate() -> Item {
-        return column(items:
+        return item(frame: Rect(x: 200, y: 200, width: 200, height: 800), items:
 			column(items:
-				collection.map { self.label(extent: Extent(width: 400, height: 50), text: $0.text) }
-			),
-			row(items:
-				button(extent: Extent(width: 200, height: 20), text: "Add", action: { _ in self.add() }),
-				button(extent: Extent(width: 200, height: 20), text: "Remove", action: { _ in self.remove() })
+				column(items:
+					collection.map { self.label(extent: Extent(width: 400, height: 50), text: $0.text) }
+				),
+				row(items:
+					button(extent: Extent(width: 200, height: 20), text: "Add", action: { _ in self.add() }),
+					button(extent: Extent(width: 200, height: 20), text: "Remove", action: { _ in self.remove() })
+				)
 			)
 		)
     }
