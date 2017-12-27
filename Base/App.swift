2129799d199d2eea4665fed1983a85fd34aaf9a9
Quentin Mathé <quentin.mathe@gmail.com>
2017-12-27 23:19:48 +0100

    Fixed app update

--- Base/App.swift
+++ Base/App.swift
@@ -9,17 +9,21 @@ import Foundation
 import RxSwift
 
 class App {
+    private let bag = DisposeBag()
 	let presentation: Presentation
 	let renderer: Renderer
-    var node: Observable<RenderedNode> {
-        return update.withLatestFrom(presentation.item).map { [unowned self] item in
-            return self.renderer.renderItem(item)
-        }
-    }
+    let node: Observable<RenderedNode>
     let update = PublishSubject<Void>()
 	
 	init(presentation: Presentation, renderer: Renderer) {
+        let item = presentation.item.map { ($0, Date()) }
+
 		self.presentation = presentation
 		self.renderer = renderer
+        self.node = update.withLatestFrom(item).distinctUntilChanged {  $0.1 == $1.1 }.map {
+            print("Render")
+            return renderer.renderItem($0.0)
+        }
+        self.node.publish().connect().disposed(by: bag)
 	}
 }
