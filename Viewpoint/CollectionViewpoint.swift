f79cb4cbd09407ecf3fd312c8079922913e7a37d
Quentin Mathé <quentin.mathe@gmail.com>
2018-01-01 19:02:52 +0100

    Moved collection viewpoint operations into extensions

--- Viewpoint/CollectionViewpoint.swift
+++ Viewpoint/CollectionViewpoint.swift
@@ -25,46 +25,6 @@ open class CollectionViewpoint<T: CreatableElement>: Presentation, SelectionStat
         public let collection: [T]
         public let changedIndexes: IndexSet
         public let selectionIndexes: IndexSet
-
-        // MARK: - Mutating State
-
-        fileprivate func adding(_ element: T) -> State {
-            let index = Int(collection.count)
-
-            return State(collection: collection.appending(element),
-                         changedIndexes: changedIndexes.inserting(index),
-                         selectionIndexes: IndexSet(integer: index))
-        }
-
-        fileprivate func removing(at index: Int) -> State {
-            let newCollection = collection.removing(at: index)
-            let empty = newCollection.isEmpty
-
-            return State(collection: newCollection,
-                         changedIndexes: changedIndexes.shifted(startingAt: index, by: -1),
-                         selectionIndexes: selectionIndexes.shifted(startingAt: index, by: -1, isEmpty: empty))
-        }
-
-        fileprivate func removing(context: Any) -> State {
-            if selectionIndexes.isEmpty {
-                print("Missing selection for remove action in /(context)")
-            }
-
-            var state = self
-            // FIXME: IndexSet(selectionIndexes).reversed() crashes, see testEnumerateReverseEmptiedSelection()
-            for index in Array(selectionIndexes).reversed() {
-                state = state.removing(at: index)
-            }
-            return state
-        }
-
-        fileprivate func selecting(_ indexes: IndexSet) -> State {
-            let adjustedChangedIndexes = changedIndexes.updatedSubset(from: selectionIndexes,
-                                                                      to: indexes)
-            return State(collection: collection,
-                         changedIndexes: adjustedChangedIndexes,
-                         selectionIndexes: indexes)
-        }
     }
 
 	public enum SelectionAdjustment {
@@ -163,32 +123,6 @@ open class CollectionViewpoint<T: CreatableElement>: Presentation, SelectionStat
         // Dummy subscription to cache the latest state, when emitting operations without any subscribers
         self.state.subscribe().disposed(by: bag)
 	}
-	
-	// MARK: - Mutating Collection
-	
-	open func createElement() -> T {
-		return T()
-	}
-
-    open func add() {
-        operation.onNext { [unowned self] in
-            $0.adding(self.createElement())
-        }
-    }
-
-    open func remove(at index: Int) {
-        operation.onNext { $0.removing(at: index) }
-    }
-	
-	open func remove() {
-        operation.onNext { [unowned self] in
-            $0.removing(context: self)
-        }
-	}
-
-    open func select(_ indexes: IndexSet) {
-        operation.onNext { $0.selecting(indexes) }
-    }
 	
 	// MARK: - Handling Changes and Visibility
 	
---
+++ Viewpoint/CollectionViewpointOperations.swift
@@ -0,0 +1,85 @@
+/**
+	Copyright (C) 2017 Quentin Mathe
+
+	Date:  July 2017
+	License:  MIT
+ */
+
+import Foundation
+import RxSwift
+import Tapestry
+
+fileprivate extension CollectionViewpoint.State {
+
+    fileprivate typealias State = CollectionViewpoint.State
+
+    // MARK: - Mutating State
+
+    fileprivate func adding(_ element: T) -> State {
+        let index = Int(collection.count)
+
+        return State(collection: collection.appending(element),
+                     changedIndexes: changedIndexes.inserting(index),
+                     selectionIndexes: IndexSet(integer: index))
+    }
+
+    fileprivate func removing(at index: Int) -> State {
+        let newCollection = collection.removing(at: index)
+        let empty = newCollection.isEmpty
+
+        return State(collection: newCollection,
+                     changedIndexes: changedIndexes.shifted(startingAt: index, by: -1),
+                     selectionIndexes: selectionIndexes.shifted(startingAt: index, by: -1, isEmpty: empty))
+    }
+
+    fileprivate func removing(context: Any) -> State {
+        if selectionIndexes.isEmpty {
+            print("Missing selection for remove action in /(context)")
+        }
+
+        var state = self
+        // FIXME: IndexSet(selectionIndexes).reversed() crashes, see testEnumerateReverseEmptiedSelection()
+        for index in Array(selectionIndexes).reversed() {
+            state = state.removing(at: index)
+        }
+        return state
+    }
+
+    fileprivate func selecting(_ indexes: IndexSet) -> State {
+        let adjustedChangedIndexes = changedIndexes.updatedSubset(from: selectionIndexes,
+                                                                  to: indexes)
+        return State(collection: collection,
+                     changedIndexes: adjustedChangedIndexes,
+                     selectionIndexes: indexes)
+    }
+}
+
+
+public extension CollectionViewpoint {
+	
+	// MARK: - Mutating Collection
+	
+	public func createElement() -> T {
+		return T()
+	}
+
+    public func add() {
+        operation.onNext { [unowned self] in
+            $0.adding(self.createElement())
+        }
+    }
+
+    public func remove(at index: Int) {
+        operation.onNext { $0.removing(at: index) }
+    }
+	
+	public func remove() {
+        operation.onNext { [unowned self] in
+            $0.removing(context: self)
+        }
+	}
+
+    public func select(_ indexes: IndexSet) {
+        operation.onNext { $0.selecting(indexes) }
+    }
+}
